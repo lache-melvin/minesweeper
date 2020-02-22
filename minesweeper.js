7fe27c43cbf0e6fcbe29b84b11feb9df2e77e0fb
Lache Melvin <lache.melvin@gmail.com>
2020-02-22 14:58:15 +1300

    add remaining mines counter and toggler

--- minesweeper.js
+++ minesweeper.js
@@ -6,9 +6,9 @@ function createBoard () {
   board.cells = []
   for (var i = 0; i < 25; i++) {
     board.cells.push({})
-    board.cells[i].row = i % 5;
-    board.cells[i].col = Math.floor(i/5);
-    board.cells[i].isMine = Math.random() < 0.4;
+    board.cells[i].row = i % 5;   //numbers rows 0-4 (repeats 5x)
+    board.cells[i].col = Math.floor(i/5);     //numbers cols 5x0, 5x1...
+    board.cells[i].isMine = Math.random() < 0.4;   //random true/false generator
     board.cells[i].hidden = true;
   }
 }
@@ -19,28 +19,18 @@ function createBoard () {
 
 
  function startGame () {
-  // Don't remove this function call: it makes the game work!
   createBoard()
-  addResetListener()
+  bindEventListeners()
+  showRemainingMines()
  
-  document.addEventListener("click", checkForWin)
-  document.addEventListener("contextmenu", checkForWin)
   for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
- 
-  
-
   lib.initBoard()
 }
 
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
+//checks after each click if the player has won the game - if yes displays win message
 function checkForWin () {
- 
- 
   var winCount = 0
   board.cells.forEach(cell => {
     if (cell.isMine && !cell.isMarked) {
@@ -53,43 +43,60 @@ function checkForWin () {
   if (winCount == board.cells.length) {
     lib.displayMessage('You win!')
   }
-  })
- 
-  
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   
+  }) 
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
+//counts number of mines surrounding a given cell
 function countSurroundingMines (cell) {
-  
-  
   var count = 0
   var surrounding = lib.getSurroundingCells(cell['row'], cell['col'])
-  
   for (var i = 0; i < surrounding.length; i++) {
     if (surrounding[i].isMine == true) {
       count++ 
     }
   }
   return count
-
-
 }
-function addResetListener () {
+
+
+function bindEventListeners () {
+  document.addEventListener("click", checkForWin)
+  document.addEventListener("contextmenu", checkForWin)
   document.getElementById('reset').addEventListener("click", boardReset)
+  document.addEventListener("contextmenu", showRemainingMines)
+  document.getElementById('minesLeftToggler').addEventListener('click', toggleRemainingMines)
+  
 }
 
+//clears board and creates a new random one
 function boardReset () {
   document.getElementsByClassName('board')[0].innerHTML = "";
   startGame();
 }
+
+
+//adds a message to top of screen with number of mines left to flag
+function showRemainingMines() {
+  var cellsWithMines = board.cells.filter (cell => {
+    return cell.isMine 
+  })
+  var unmarkedMines = cellsWithMines.filter (cell => {
+    return !cell.isMarked
+  }).length
+  if (unmarkedMines > 1) {
+    document.getElementById('bombsRemaining').innerHTML =  unmarkedMines + " mines remaining"
+  }
+  else if (unmarkedMines == 1) {
+    document.getElementById('bombsRemaining').innerHTML =  "1 mine remaining"
+  } else {
+    document.getElementById('bombsRemaining').innerHTML =  "0 mines remaining - you did it!"
+  }
+}
+
+function toggleRemainingMines() {
+  document.getElementById('bombsRemaining').classList.toggle('remainingMines')
+}
+
+
+
+
